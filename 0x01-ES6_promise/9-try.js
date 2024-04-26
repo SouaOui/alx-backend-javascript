88f6ec0fbc832474bc1313ac458c3f2b7226e4ad
Buguster <your.email@example.com>
2024-04-26 21:42:50 +0100

    Ecmascript Promises

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,10 +1,11 @@
 export default function guardrail(mathFunction) {
-  return new Promise((resolve) => {
-    try {
-      const result = mathFunction();
-      resolve([result, 'Guardrail was processed']);
-    } catch (error) {
-      resolve([error.message, 'Guardrail was processed']);
-    }
-  });
+  const queue = [];
+  try {
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(String(err));
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
 }
