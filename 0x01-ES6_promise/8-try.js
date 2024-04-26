03a9f31746346c5ca2f2a670044fef257880ca22
Buguster <your.email@example.com>
2024-04-26 14:06:13 +0100

    Ecmascript Promises

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,10 +1,7 @@
 export default function divideFunction(numerator, denominator) {
-  return new Promise((resolve, reject) => {
-    if (denominator !== 0) {
-      const result = numerator / denominator;
-      resolve(result);
-    } else {
-      reject(new Error('Cannot divide by 0'));
-    }
-  });
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
+  } else {
+    return numerator / denominator;
+  }
 }
