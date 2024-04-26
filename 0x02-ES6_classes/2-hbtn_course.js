92a5bb44accf3c5c2938e41bad101ffa89ce91fb
Buguster <your.email@example.com>
2024-04-26 23:34:05 +0100

    Ecmascript Classes

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,54 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (
+      typeof name !== 'string'
+      || typeof length !== 'number'
+      || !Array.isArray(students)
+      || !students.every((student) => typeof student === 'string')
+    ) {
+      throw new Error('Invalid input types');
+    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  // Getters and Setters Name
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  // Getters and Setters Length
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw new TypeError('Length must be a valid number');
+    }
+    this._length = newLength;
+  }
+
+  // Getters and Setters Students
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (
+      !Array.isArray(newStudents)
+      || !newStudents.every((item) => typeof item === 'string')
+    ) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = newStudents;
+  }
+}
