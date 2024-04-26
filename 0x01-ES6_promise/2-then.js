deacca947588d05e3a9ecb865a33dd115e179b98
Buguster <your.email@example.com>
2024-04-26 12:24:43 +0100

    Ecmascript Promises

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,1 +1,14 @@
-function handleResponseFromAPI(promise) 
+function handleResponseFromAPI(promise) {
+  return promise
+    .then((response) => {
+      console.log("Got a response from the API");
+      return {
+        status: 200,
+        body: "success",
+      };
+    })
+    .catch((error) => {
+      return new Error();
+    });
+}
+export default handleResponseFromAPI;
