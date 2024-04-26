f858ee360a1ac6a42ada07d57461c4cbe83af890
SouaOui <your.email@example.com>
2024-04-27 00:32:21 +0100

    Ecmascript Classes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,12 +1,12 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
     if (
-      typeof name !== 'string'
-      || typeof length !== 'number'
-      || !Array.isArray(students)
-      || !students.every((student) => typeof student === 'string')
+      typeof name !== "string" ||
+      typeof length !== "number" ||
+      !Array.isArray(students) ||
+      !students.every((student) => typeof student === "string")
     ) {
-      throw new Error('Invalid input types');
+      throw new Error("Invalid input types");
     }
     this._name = name;
     this._length = length;
@@ -19,20 +19,19 @@ export default class HolbertonCourse {
   }
 
   set name(newName) {
-    if (typeof newName !== 'string') {
-      throw new TypeError('Name must be a string');
+    if (typeof newName !== "string") {
+      throw new TypeError("Name must be a string");
     }
     this._name = newName;
   }
 
-  // Getters and Setters Length
   get length() {
     return this._length;
   }
 
   set length(newLength) {
-    if (typeof newLength !== 'number') {
-      throw new TypeError('Length must be a valid number');
+    if (typeof newLength !== "number") {
+      throw new TypeError("Length must be a valid number");
     }
     this._length = newLength;
   }
@@ -44,10 +43,10 @@ export default class HolbertonCourse {
 
   set students(newStudents) {
     if (
-      !Array.isArray(newStudents)
-      || !newStudents.every((item) => typeof item === 'string')
+      !Array.isArray(newStudents) ||
+      !newStudents.every((item) => typeof item === "string")
     ) {
-      throw new TypeError('Students must be an array of strings');
+      throw new TypeError("Students must be an array of strings");
     }
     this._students = newStudents;
   }
