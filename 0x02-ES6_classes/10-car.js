14d1c3b418a436190d7f51e264cd20099fc51d0b
SouaOui <your.email@example.com>
2024-04-27 00:42:34 +0100

    Ecmascript Classes

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,28 +1,12 @@
-const brandSymbol = Symbol('brand');
-const motorSymbol = Symbol('motor');
-const colorSymbol = Symbol('color');
-
 export default class Car {
   constructor(brand, motor, color) {
-    this[brandSymbol] = brand;
-    this[motorSymbol] = motor;
-    this[colorSymbol] = color;
-  }
-
-  // Getter methods
-  get brand() {
-    return this[brandSymbol];
-  }
-
-  get motor() {
-    return this[motorSymbol];
-  }
-
-  get color() {
-    return this[colorSymbol];
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
   cloneCar() {
-    return new Car(this[brandSymbol], this[motorSymbol], this[colorSymbol]);
+    // Using spread syntax to create a new object with copied properties
+    return new Car(...Object.values(this));
   }
 }
