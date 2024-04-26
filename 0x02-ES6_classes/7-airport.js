fa8186f7ca0badf3b48dd5a9018904daa00220b3
Buguster <your.email@example.com>
2024-04-27 00:21:27 +0100

    Ecmascript Classes

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -10,8 +10,8 @@ export default class Airport {
   }
 
   set code(newCode) {
-    if (typeof newCode !== "string") {
-      throw new TypeError("Name must be a string");
+    if (typeof newCode !== 'string') {
+      throw new TypeError('Name must be a string');
     }
     this._code = newCode;
   }
@@ -21,8 +21,8 @@ export default class Airport {
   }
 
   set name(newName) {
-    if (typeof newName !== "string") {
-      throw new TypeError("Name must be a string");
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
     }
     this._name = newName;
   }
