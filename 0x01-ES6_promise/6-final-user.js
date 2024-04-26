a9c547d87198b598c76aca47d9af756679e097b6
Buguster <your.email@example.com>
2024-04-26 13:48:25 +0100

    Ecmascript Promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,44 +1,19 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
 
 export default async function handleProfileSignup(
   firstName,
   lastName,
-  fileName,
+  fileName
 ) {
-  const signUpUserPromise = signUpUser(firstName, lastName);
-  const uploadPhotoPromise = uploadPhoto(fileName);
-
-  const [userData, photoData] = await Promise.allSettled([
-    signUpUserPromise,
-    uploadPhotoPromise,
-  ]);
-
-  const results = [];
-
-  if (userData.status === 'fulfilled') {
-    results.push({
-      status: 'fulfilled',
-      value: userData.value,
-    });
-  } else {
-    results.push({
-      status: 'rejected',
-      value: userData.reason.message,
-    });
-  }
-
-  if (photoData.status === 'fulfilled') {
-    results.push({
-      status: 'fulfilled',
-      value: 'Photo uploaded successfully',
-    });
-  } else {
-    results.push({
-      status: 'rejected',
-      value: photoData.reason.message,
-    });
-  }
-
-  return results;
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((res) =>
+    res.map((result) => ({
+      status: result.status,
+      value:
+        result.status === "fulfilled" ? result.value : String(result.reason),
+    }))
+  );
 }
