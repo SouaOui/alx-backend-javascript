675707b7979051a60fcdef852d0175fb0d7ecbda
SouaOui <your.email@example.com>
2024-04-30 14:35:33 +0100

    Ecmascript Data Manipulation

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,9 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position > length) {
+    throw new Error("Position outside range");
+  }
+  const array = new ArrayBuffer(length);
+  const ArrayView = new DataView(array);
+  ArrayView.setInt8(position, value);
+  return ArrayView;
+}
