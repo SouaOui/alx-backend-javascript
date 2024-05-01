655ca7390a935a01f89f793e1c02d38451fe8c78
SouaOui <your.email@example.com>
2024-05-01 11:08:21 +0100

    Typescript

---
+++ 0x04-TypeScript/task_1/js/main.js
@@ -0,0 +1,13 @@
+var student1 = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 20,
+    location: "New York"
+};
+var student2 = {
+    firstName: "Jane",
+    lastName: "Smith",
+    age: 22,
+    location: "Los Angeles"
+};
+var studentsList = [student1, student2];
