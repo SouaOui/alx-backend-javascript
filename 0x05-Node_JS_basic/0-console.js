049039576b40efecc12dfdf099943d03de241f34
SouaOui <your.email@example.com>
2024-08-24 00:07:02 +0100

    NodeJS_Basic

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,10 @@
-const displayMessage = (MsgContent) => {
-  console.log(MsgContent);
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
 };
 
 module.exports = displayMessage;
--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,30 +1,13 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
-if (process.stdin.isTTY) {
-  process.stdin.on("data", (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-} else {
-  process.stdin.on("data", (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-  process.on("exit", () => {
-    console.log("This important software is now closing");
-  });
-}
-// process.nextTick(() => {
-//   console.log("Got triggered in the first iteration of event loop");
-// });
-// process.nextTick(() => {
-//   console.log("Got triggered in the second iteration of event loop");
-// });
-// setTimeout(() => {
-//   console.log("Even after nextTick is executed");
-// }, 1000);
-// console.log("First text to be printed");
-// const process = require("process");
-// console.log(process.argv);
-// var fs = require("fs");
-// fs.createReadStream(__filename).pipe(process.stdout);
-// console.log(__filename);
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
