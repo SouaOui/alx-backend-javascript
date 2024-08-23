d699cb7862b451f0ac9ea3013e4dcb38ebb0fb2b
SouaOui <your.email@example.com>
2024-08-23 13:04:14 +0100

    2. Reading a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,88 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+
+function countStudents (filepath) {
+  if (!filepath) {
+    throw new Error('Cannot load the database');
+  }
+
+  const studentsByField = {};
+  let totalStudents = 0;
+
+  try {
+    fs.createReadStream(filepath)
+      .pipe(csv())
+      .on('data', (data) => {
+        const { firstname, field } = data;
+
+        if (!studentsByField[field]) {
+          studentsByField[field] = {
+            count: 0,
+            students: []
+          };
+        }
+
+        studentsByField[field].count++;
+        studentsByField[field].students.push(firstname);
+        totalStudents++;
+      })
+      .on('end', () => {
+        console.log(`Number of students: ${totalStudents}`);
+
+        for (const field in studentsByField) {
+          const { count, students } = studentsByField[field];
+          console.log(
+            `Number of students in ${field}: ${count}. List: ${students.join(
+              ', '
+            )}`
+          );
+        }
+      });
+  } catch (error) {
+    console.error('Error reading or processing file:', error);
+  }
+}
+
+module.exports = countStudents;
+// function countStudents(filepath) {
+//   const fs = require("fs");
+//   const csv = require("csv-parser");
+
+//   const studentsByField = {};
+
+//   if (!filepath) {
+//     throw "Cannot load the database";
+//   } else {
+//     FileBuffer = fs.readFileSync(filepath);
+//     to_string = FileBuffer.toString();
+//     splitLines = to_string.split("\n");
+//     const NumberLines = splitLines.length - 1;
+//     console.log(`Number of students: ${NumberLines}`);
+//   }
+//   fs.createReadStream(filepath)
+//     .pipe(csv())
+//     .on("data", (data) => {
+//       const { firstname, field } = data;
+
+//       if (!studentsByField[field]) {
+//         studentsByField[field] = {
+//           count: 0,
+//           students: [],
+//         };
+//       }
+
+//       studentsByField[field].count++;
+//       studentsByField[field].students.push(firstname);
+//     })
+//     .on("end", () => {
+//       for (const field in studentsByField) {
+//         const { count, students } = studentsByField[field];
+//         console.log(
+//           `Number of students in ${field}: ${count}. List: ${students.join(
+//             ", "
+//           )}`
+//         );
+//       }
+//     });
+// }
+// module.exports = countStudents;
